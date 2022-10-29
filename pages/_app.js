5694d58fa7e431c2e8d94a823122ec01afcafc13
Aditya Bhattad <adityabhattad18@gmail.com>
2022-10-29 22:48:42 +0530

    Connect Wallet Done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
 import '../styles/globals.css'
+import { Web3Modal } from '@web3modal/react'
+import { chains, providers } from "@web3modal/ethereum";
+
+
+
+
+const config = {
+  projectId: process.env.WALLET_CONNECT_PROJECT_ID,
+  theme: "dark",
+  accentColor: "green",
+  ethereum: {
+    appName: 'web3Modal',
+    chains: [
+      chains.localhost,chains.goerli
+    ],
+    providers: [
+      providers.walletConnectProvider({projectId:process.env.WALLET_CONNECT_PROJECT_ID})
+    ],
+    autoConnect: true
+  }
+};
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Web3Modal config={config} />
+    </>
+  )
 }
 
 export default MyApp
