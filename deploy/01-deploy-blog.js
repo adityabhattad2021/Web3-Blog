1db3c8844bac806f849458d647bc55d8099fbb18
Aditya Bhattad <adityabhattad18@gmail.com>
2022-10-29 18:19:59 +0530

    Contract Address for frontend

--- deploy/01-deploy-blog.js
+++ deploy/01-deploy-blog.js
@@ -1,21 +1,30 @@
 const { network, ethers } = require("hardhat");
+const fs = require("fs");
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
-    const { deploy, log } = deployments;
-    const { deployer } = await getNamedAccounts();
-    log(`---------deployer is ${deployer}---------`);
+	const { deploy, log } = deployments;
+	const { deployer } = await getNamedAccounts();
+	log(`---------deployer is ${deployer}---------`);
 
-    const name = "Sample Blog";
-    const args = [name]
-    log("-----------------------Deploying Blog-----------------------")
-    const blog = await deploy("Blog", {
-        from: deployer,
-        args: args,
-        log: true,
-        waitConfirmations: network.config.blockConfirmations || 1
-    });
+	const name = "Sample Blog";
+	const args = [name];
+	log("-----------------------Deploying Blog-----------------------");
+	const blog = await deploy("Blog", {
+		from: deployer,
+		args: args,
+		log: true,
+		waitConfirmations: network.config.blockConfirmations || 1,
+	});
     log("-----------------------Deployed Successfully----------------");
+    log(blog.address)
 
-}
+	log("---------------------Writing ABI------------------------");
+	fs.writeFileSync(
+        "./config.js",`
+    export const contractAddress = "${blog.address}";
+    export const ownerAddress = "${blog.receipt.from}";
+    `);
+	log("--------------------------------------------------------");
+};
 
 module.exports.tags = ["all"];
