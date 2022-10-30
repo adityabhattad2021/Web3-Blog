c28187b6ca0acde6c0a12b9ba1f90b9cf80046cd
Aditya Bhattad <adityabhattad18@gmail.com>
2022-10-30 21:19:31 +0530

    Upload Image done

--- components/PostPage.js
+++ components/PostPage.js
@@ -1,67 +1,57 @@
-import { css } from '@emotion/css'
-import { useContext } from 'react'
-import { useRouter } from 'next/router'
-import { ethers } from 'ethers'
-import Link from 'next/link'
-import { AccountContext } from '../context'
+import { css } from "@emotion/css";
+import { useContext } from "react";
+import { useRouter } from "next/router";
+import { ethers } from "ethers";
+import Link from "next/link";
+import { AccountContext } from "../context";
 
 /* import contract address and contract owner address */
-import {
-  contractAddress, ownerAddress
-} from '../config'
+import { contractAddress, ownerAddress } from "../config";
 
-
-import Blog from '../artifacts/contracts/Blog.sol/Blog.json'
-
+import Blog from "../artifacts/contracts/Blog.sol/Blog.json";
 
 export default function PostPage(props) {
+	const { posts } = props;
+	const account = useContext(AccountContext);
 
-    const { posts } = props
-    const account = useContext(AccountContext)
-
-    const router = useRouter()
-    async function navigate() {
-		router.push("/create-post")
+	const router = useRouter();
+	async function navigate() {
+		router.push("/create-post");
 	}
 
-
-
-    return (
-        <div >
-				<div className={postList}>
-					{
-                    posts && posts.map((post) => (
-                            <Link href={`/post/${post[2]}`} key={index}>
-                                <div className={linkStyle}>
-                                    <p className={postTitle}>{post[1]}</p>
-                                    <div className={arrowContainer}>
-                                        <img
-                                            src='/right-arrow.svg'
-                                            alt='right-arrow'
-                                            className={smallArrow}
-                                        />
-                                    </div>
-                                </div>
-                            </Link>
-						))	
-					}
-				</div>
-				<div className={container}>
-					{
-						account.address === ownerAddress && (
-							<button className={buttonStyle} onClick={navigate}>
-								Create The First Post
-								<img
-									src='/right-arrow.svg'
-									alt='right-arrow'
-									className={arrow}
-								/>
-							</button>
-						)
-					}
-				</div>
+	return (
+		<div>
+			<div className={postList}>
+				{posts &&
+					posts.map((post) => (
+						<Link href={`/post/${post[2]}`} key={index}>
+							<div className={linkStyle}>
+								<p className={postTitle}>{post[1]}</p>
+								<div className={arrowContainer}>
+									<img
+										src="/right-arrow.svg"
+										alt="right-arrow"
+										className={smallArrow}
+									/>
+								</div>
+							</div>
+						</Link>
+					))}
 			</div>
-    )
+			<div className={container}>
+				{account.address === ownerAddress && (
+					<button className={buttonStyle} onClick={navigate}>
+						Create The First Post
+						<img
+							src="/right-arrow.svg"
+							alt="right-arrow"
+							className={arrow}
+						/>
+					</button>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export async function getServerSideProps() {
@@ -71,65 +61,64 @@ export async function getServerSideProps() {
 	console.log(posts);
 	return {
 		props: {
-			posts:JSON.parse(JSON.stringify(posts)),
+			posts: JSON.parse(JSON.stringify(posts)),
 		},
 	};
 }
 
-
 const arrowContainer = css`
-  display: flex;
-  flex: 1;
-  justify-content: flex-end;
-  padding-right: 20px;
-`
+	display: flex;
+	flex: 1;
+	justify-content: flex-end;
+	padding-right: 20px;
+`;
 
 const postTitle = css`
-  font-size: 30px;
-  font-weight: bold;
-  cursor: pointer;
-  margin: 0;
-  padding: 20px;
-`
+	font-size: 30px;
+	font-weight: bold;
+	cursor: pointer;
+	margin: 0;
+	padding: 20px;
+`;
 
 const linkStyle = css`
-  border: 1px solid #ddd;
-  margin-top: 20px;
-  border-radius: 8px;
-  display: flex;
-`
+	border: 1px solid #ddd;
+	margin-top: 20px;
+	border-radius: 8px;
+	display: flex;
+`;
 
 const postList = css`
-  width: 700px;
-  margin: 0 auto;
-  padding-top: 50px;  
-`
+	width: 700px;
+	margin: 0 auto;
+	padding-top: 50px;
+`;
 
 const container = css`
-  display: flex;
-  justify-content: center;
-`
+	display: flex;
+	justify-content: center;
+`;
 
 const buttonStyle = css`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-top: 100px;
-  background-color: #fafafa;
-  outline: none;
-  border: none;
-  font-size: 44px;
-  padding: 20px 40px;
-  border-radius: 15px;
-  cursor: pointer;
-  box-shadow: 7px 7px rgba(0, 0, 0, .1);
-`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	margin-top: 100px;
+	background-color: #fafafa;
+	outline: none;
+	border: none;
+	font-size: 44px;
+	padding: 20px 40px;
+	border-radius: 15px;
+	cursor: pointer;
+	box-shadow: 7px 7px rgba(0, 0, 0, 0.1);
+`;
 
 const arrow = css`
-  width: 35px;
-  margin-left: 30px;
-`
+	width: 35px;
+	margin-left: 30px;
+`;
 
 const smallArrow = css`
-  width: 25px;
-`
+	width: 25px;
+`;
---
+++ pages/create-post.js
@@ -0,0 +1,184 @@
+import { useState, useRef, useEffect } from "react";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import { css } from "@emotion/css";
+import { ethers } from "ethers";
+// import { create } from 'ipfs-http-client/dist/src'
+import { Web3Storage, getFilesFromPath } from "web3.storage";
+
+import { contractAddress, ownerAddress } from "../config";
+import Blog from "../artifacts/contracts/Blog.sol/Blog.json";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+	ssr: false,
+});
+
+const initialState = { title: "", content: "" };
+
+const token =process.env.NEXT_PUBLIC_WEB3_STORAGE
+	;
+
+export default function CreatePost() {
+	const router = useRouter();
+	const [post, setPost] = useState(initialState);
+	const [image, setImage] = useState(null);
+	const [loaded, setLoaded] = useState(false);
+
+	const fileRef = useRef(null);
+
+	const storage = new Web3Storage({ token });
+	const { title, content } = post;
+
+	useEffect(() => {
+		setTimeout(() => {
+			setLoaded(true);
+		}, 500);
+	}, []);
+
+	function onChange(e) {
+		setPost(() => ({ ...post, [e.target.name]: e.target.value }));
+	}
+
+	async function savePostToIPFS() {
+		try {
+			const cid = await storage.put(post)
+			return cid;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	async function savePost(hash) {
+		if (typeof window.ethereum !== "undefined") {
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
+			const signer = provider.getSigner();
+			const contract = new ethers.Contract(
+				contractAddress,
+				Blog.abi,
+				signer
+			);
+
+			try {
+				const val = await contract.createPost(post.title, hash);
+				console.log("Val: ", val);
+			} catch (error) {
+				console.log("Error: ", error);
+			}
+		}
+	}
+
+	async function createNewPost() {
+		if (!title || !content) return;
+
+		const hash = savePostToIPFS();
+		await savePost(hash);
+		router.push("/");
+	}
+
+	async function handleFileChange(e) {
+		const uploadedFile = e.target.files[0];
+		console.log(uploadedFile);
+		if (!uploadedFile) {
+			console.log("No uploaded file found");
+			return;
+		}
+
+		const cid = await storage.put([uploadedFile])	
+
+		console.log(cid);
+
+		setPost((state) => ({ ...state, coverImage: cid }));
+	}
+
+	function triggerOnChange() {
+		fileRef.current.click();
+	}
+
+	return (
+		<div className={container}>
+			{image && (
+				<img
+					className={coverImageStyle}
+					src={URL.createObjectURL(image)}
+				/>
+			)}
+			<input
+				onChange={onChange}
+				name="title"
+				placeholder="Give it a title..."
+				value={post.title}
+				className={titleStyle}
+			/>
+			<SimpleMDE
+				className={mdEditor}
+				placeholder="Write something here..."
+				value={post.content}
+				onChange={(value) => setPost({ ...post, content: value })}
+			/>
+			{loaded && (
+				<>
+					<button
+						className={button}
+						type="button"
+						onClick={createNewPost}
+					>
+						Publish
+					</button>
+					<button
+						onClick={triggerOnChange} className={button}
+					>
+						Add Cover Image
+					</button>
+				</>
+			)}
+			<input
+				id="selectImage"
+				className={hiddenInput}
+				type="file"
+				onChange={handleFileChange}
+				ref={fileRef}
+			/>
+		</div>
+	);
+}
+
+const hiddenInput = css`
+	display: none;
+`;
+
+const coverImageStyle = css`
+	max-width: 800px;
+`;
+
+const mdEditor = css`
+	margin-top: 40px;
+`;
+
+const titleStyle = css`
+	margin-top: 40px;
+	border: none;
+	outline: none;
+	background-color: inherit;
+	font-size: 44px;
+	font-weight: 600;
+	&::placeholder {
+		color: #999999;
+	}
+`;
+
+const container = css`
+	width: 800px;
+	margin: 0 auto;
+`;
+
+const button = css`
+	background-color: #fafafa;
+	outline: none;
+	border: none;
+	border-radius: 15px;
+	cursor: pointer;
+	margin-right: 10px;
+	font-size: 18px;
+	padding: 16px 70px;
+	box-shadow: 7px 7px rgba(0, 0, 0, 0.1);
+`;
